ff92bf7c22c610a16101053e882bae6ff845173e
nelsonic <contact.nelsonic+github@gmail.com>
2016-01-22 16:47:10 +0000

    adds .png image so we can include a test image in email. https://github.com/nelsonic/idea2/issues/1

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var http  = require('http');
 var hits  = require('./lib/hits');
 var port  = process.env.PORT || 8000;
 var wreck = require('wreck');
-
+var fs    = require('fs');
+var png   = fs.readFileSync('./image_50x50.png');
 http.createServer(function handler(req, res) {
   var url = req.url;
   var agent = req.headers.user-agent
   var r = req.headers;
   r.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  r.url = url.replace('.svg', '');
-
+  r.url = url.replace('.svg', '').replace('.png', '');
+  console.log('>>'+url);
   if (url.match(/svg/)) {
     hits.add(r, function(err, count) {
-      console.log(r.url, ' >> ', count)
+      console.log(r.url, ' >> ', count);
       var newurl = "https://img.shields.io/badge/hits-" + count +"-brightgreen.svg"
       wreck.get(newurl, function (error, response, html) {
         // expiry headers see: http://stackoverflow.com/a/2068407/1148249
@@ -26,6 +27,25 @@ http.createServer(function handler(req, res) {
         res.writeHead(200, head);
         res.end(html);
       });
+    })
+
+  }
+  else if (url.match(/png/)) {
+    hits.add(r, function(err, count) {
+      console.log(r.url, ' >> ', count)
+      // var newurl = "https://img.shields.io/badge/hits-" + count +"-brightgreen.png"
+      // wreck.get(newurl, function (error, response, html) {
+        // expiry headers see: http://stackoverflow.com/a/2068407/1148249
+        // console.og()
+        var head = {
+          "Cache-Control": "no-cache, no-store, must-revalidate", // HTTP 1.1
+          "Pragma": "no-cache",                                   // HTTP 1.0
+          "Expires": "0",                                         // Proxies
+          "Content-Type":"image/png"
+        }
+        res.writeHead(200, head);
+        res.end(png);
+      // });
     })
 
   }
