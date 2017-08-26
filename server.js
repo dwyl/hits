50131ced3f64765849750b2d2161c5854f75c449
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-26 21:54:09 +0100

    tidy up server.js and revive socket.io "live updates" https://github.com/dwyl/hits/issues/17

--- server.js
+++ server.js
@@ -1,72 +1,54 @@
 var port  = process.env.PORT || 8000;
 var http  = require('http'); // plain http server (no fancy framework required)
 var fs    = require('fs'); // so we can open the file
-var png   = fs.readFileSync('./lib/1x1px.png'); // "tracking pixel" 
 var hits  = require('./lib/hits'); // our storage interface
-var favicon = 'http://i.imgur.com/zBEQq4w.png'; // dwyl favicon
 var make_svg = require('./lib/make_svg.js');
 var extract = require('./lib/extract_request_data.js');
+var FAVICON = 'http://i.imgur.com/zBEQq4w.png'; // dwyl favicon
 var HEAD = require('./lib/headers.json'); // stackoverflow.com/a/2068407/1148249
 
-var app = http.createServer(function handler(req, res) {
-  
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+app.listen(port);
+
+function handler (req, res) {
   var url = req.url;
-  console.log(url);
+  var hit = extract(req);
+  console.log(hit);
   if (url.match(/svg/)) {
-    var hit = extract(req);
-    console.log('hit:', hit);
     hits(hit, function(err, count) {
+      // var 
+      io.sockets.emit('hit', { 'hit': hit });
       console.log(url, ' >> ', count);
       res.writeHead(200, HEAD);
       res.end(make_svg(count));
     });
   }
-  // else if (url.match(/png/)) { // see: https://github.com/dwyl/hits/issues/4
-  //   hits.add(r, function(err, count) {
-  //     console.log(r.url, ' >> ', count);
-  //     res.writeHead(200, Object.assign(HEAD, {"Content-Type": "image/png"}));
-  //     res.end(png);
-  //   });
-  // }
   else if(url === '/favicon.ico') {
-    res.writeHead(301, { "Location": favicon });
+    console.log('favicon.ico');
+    res.writeHead(301, { "Location": FAVICON });
     res.end();
   }
-  else if(url === '/stats') {
-    fs.readFile('./lib/index.html', 'utf8', function (err, data) {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(data);
-    });
-  }
-  else if(url === '/client.js') {
+  else if(url === '/client.js') { // these can be cached in "Prod" ...
     fs.readFile('./lib/client.js', 'utf8', function (err, data) {
       res.writeHead(200, {"Content-Type": "application/javascript"});
       res.end(data);
     });
   }
-  else if(url === '/style.css') {
-    fs.readFile('./lib/style.css', 'utf8', function (err, data) {
-      res.writeHead(200, {"Content-Type": "text/css"});
+  else { // echo the record without saving it
+    fs.readFile('./lib/index.html', 'utf8', function (err, data) {
+      res.writeHead(200, {"Content-Type": "text/html"});
       res.end(data);
     });
   }
-  else { // echo the record without saving it
-    console.log(" - - - - - - - - - - record:", r);
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(r, null, "  "));
-  } // pretty JSON in Browser see: http://stackoverflow.com/a/5523967/1148249
-}).listen(port);
-
-var io = require('socket.io')(app);
-
-io.on('connection', function (socket) {
-  console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ');
-  console.log(socket.client.conn);
-  console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ');
-  socket.emit('news', { msg: 'welcome to stats-ville!' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+}
 
 console.log('Visit http://localhost:' + port);
