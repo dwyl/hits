ba8a596f0cca0eabe8726241833fe5a40bf6de52
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-07 22:31:59 +0100

    adds route for index.html and favicon.ico in lib/router.ex

--- lib/router.ex
+++ lib/router.ex
@@ -4,11 +4,28 @@ defmodule App.Router do
   plug :match
   plug :dispatch
 
-  get "/", do: send_resp(conn, 200, "Welcome")
+  get "/", do: send_file(conn, 200, "lib/index.html")
+  get "/favicon.ico", do: send_file(conn, 200, "lib/favicon.ico")
+  
+  # get "/favicon.ico" do 
+  #   
+  #   send_resp(conn, 301,  "http://i.imgur.com/zBEQq4w.png")
+  # end
 
   match _ do 
-    IO.inspect conn.path_info
-    IO.inspect Enum.join(conn.path_info, "/")
-    send_resp(conn, 404, Enum.join(conn.path_info, "/"))
+    path = conn.path_info
+    cond do
+      # Enum.member?(path, "favicon.ico") ->
+      #   IO.puts "FAVICON!"
+      # 
+      # Enum.member?(path, "favicon.ico") ->
+      #   IO.puts "FAVICON!"
+      #   reply = put_resp_content_type(conn, "image/x-icon")
+      #   send_file(reply, 200, "lib/favicon.ico")
+      true ->
+        IO.inspect conn.path_info
+        IO.inspect Enum.join(conn.path_info, "/")
+        send_resp(conn, 404, Enum.join(conn.path_info, "/"))  
+    end
   end
 end
