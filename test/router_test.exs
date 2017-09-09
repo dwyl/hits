4956877e1ee99a879fca49e2e12e1b15d0041a50
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-09 17:48:16 +0100

    fix dogma errors noted in https://github.com/dwyl/hits-elixir/issues/4#issuecomment-328289094

--- test/router_test.exs
+++ test/router_test.exs
@@ -21,15 +21,14 @@ defmodule App.RouterTest do
     assert conn.state == :sent
     assert conn.status == 200
   end
-  
+
   test "Request SVG Endpoint returns 200" do
     conn = conn(:get, "/org/" <>
       Integer.to_string(:rand.uniform(1000000)) <> ".svg")
     conn = put_req_header(conn, "user-agent", "Hackintosh")
     conn = put_req_header(conn, "accept-language", "en-GB,en;q=0.5")
     conn = Router.call(conn, @opts)
-    # IO.inspect conn.resp_body
-    assert conn.resp_body =~ "<text x=\"54\" y=\"14\">1</text>"
+    assert conn.resp_body =~ ~s(<text x=\"54\" y=\"14\">1</text>)
     assert conn.state == :sent
     assert conn.status == 200
   end
