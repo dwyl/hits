e5abc4961894328f5ea1e3867eaaa3dc07b5e9f8
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-09 11:52:50 +0100

    adds dev dependencies for running coveralls & dogma

--- mix.exs
+++ mix.exs
@@ -26,7 +26,10 @@ defmodule App.Mixfile do # this is a fairly standard/simple mix file ...
       {:cowboy, "~> 1.1.2"}, # the Web Server that runs our Elixir App
       {:plug, "~> 1.3.4"},   # Elixir's HTTP library for routing etc.
       {:poison, "~> 3.1"}, # to decode .json file in hash test
-      {:json, "~> 1.0"}
+      {:json, "~> 1.0", only: :dev}, # for reading JSON fixture file
+      {:excoveralls, "~> 0.6.2", only: :dev}, # tracking test coverage
+      {:ex_doc, "~> 0.11", only: :dev}, # to generate documentation
+      {:dogma, "~> 0.1", only: :dev}, # Elixir style
     ]
   end
 end
