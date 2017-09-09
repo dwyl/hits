6a19d1f6cbc8bdb2facfc9c128c401f158ad3150
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-09 18:19:08 +0100

    adds second test to check counter increases fixes https://github.com/dwyl/hits-elixir/issues/4#issuecomment-328291294

--- test/router_test.exs
+++ test/router_test.exs
@@ -36,12 +36,20 @@ defmodule App.RouterTest do
     conn = conn(:get, "/org/" <>
       Integer.to_string(:rand.uniform(1000000)) <> ".svg")
     conn = put_req_header(conn, "user-agent", "Hackintosh")
-    conn = put_req_header(conn, "accept-language", "en-GB,en;q=0.5")
-    conn = Router.call(conn, @opts)
+    req = put_req_header(conn, "accept-language", "en-GB,en;q=0.5")
+    conn = Router.call(req, @opts)
     assert conn.resp_body =~ ~s(<text x=\"54\" y=\"14\">1</text>)
+
     assert conn.state == :sent
     assert conn.status == 200
+
+    # Second Call to check Count increased
+    conn = Router.call(req, @opts)
+    assert conn.resp_body =~ ~s(<text x=\"54\" y=\"14\">2</text>)
   end
+
+
+
 
   test "returns 404" do
     route = conn(:get, "/missing", "")
