9416b5fbfd8b4b80d00b1aacb2f4fbcd2fd686d7
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-11 20:37:59 +0100

    adds function-level docs for Hash.make function fixes #8

--- lib/hash.ex
+++ lib/hash.ex
@@ -2,6 +2,20 @@ defmodule Hash do
   @moduledoc """
   Returns a SHA512 transformed to Base64, remove ambiguous chars then sub-string
   """
+  
+  @doc """
+  make/2 create a SHA512 hash from the given input and return the require length
+  note: we remove "ambiguous" characters so _humans_ can type the hash without
+  getting "confused" this might not be required, but is to match the original 
+  "Hits" implementation.
+  
+  ## Parameters
+
+  - input: String the string to be hashed.
+  - length: Number the length of string required
+  
+  Returns String hash of desired length.
+  """
   def make(input, length \\ 13) do
     hash = :crypto.hash(:sha512, input) # dogma requires this extra line ... =(
     hash
