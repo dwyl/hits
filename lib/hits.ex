59d453cebe563ad02dfe748a0e772b25e558d11c
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-11 20:33:02 +0100

    adds function-level docs to all functions in lib/hits.ex for https://github.com/dwyl/hits-elixir/issues/8

--- lib/hits.ex
+++ lib/hits.ex
@@ -4,17 +4,42 @@ defmodule App.Hits do
   """
   require Hash
 
-  # open the SVG template file
+  @doc """
+  svg_badge_template/0 opens the SVG template file.
+  the function is single-purpose so that we can _cache_ the template.
+  see: https://github.com/dwyl/hits-elixir/issues/3 #helpwanted
+  
+  returns String of template.
+  """
   def svg_badge_template do # help wanted caching this!
     File.read!("./lib/template.svg") # see: github.com/dwyl/hits-elixir/issues/3
   end
 
-  # make_badge from svg template substituting the count value
+  @doc """
+  make_badge/1 from svg template substituting the count value
+  
+  ## Parameters
+
+  - count: Number the view/hit count to be displayed in the badge.
+  
+  Returns the badge XML with the count.
+  """
   def make_badge(count \\ 1) do
     String.replace(svg_badge_template(), ~r/{count}/, to_string(count))
   end
 
-  # there is probably a *much* better way of doing this ... PR v. welcome!
+  @doc """
+  get_user_agent_string/1 extracts user-agent, IP address and browser language
+  from the Plug.Conn map see: 
+  
+  > there is probably a *much* better way of doing this ... PR v. welcome!
+  
+  ## Parameters
+
+  - conn: Map the standard Plug.Conn info see: hexdocs.pm/plug/Plug.Conn.html
+  
+  Returns String with user-agent, IP and language separated by "pipe" charater.
+  """
   def get_user_agent_string(conn) do
     [{_, ua}] = Enum.filter(conn.req_headers, fn
       {k, _} -> k == "user-agent" end)
@@ -26,7 +51,19 @@ defmodule App.Hits do
     Enum.join([ua, ip, lang], "|")
   end
 
-  # save/overwrite user-agent in a file with the hash as filename
+  @doc """
+  save_user_agent_has/1 save/overwrite user-agent in a file 
+  the filename is the SHA hash of the string so it's always the same.
+  from the Plug.Conn map see: https://hexdocs.pm/plug/Plug.Conn.html
+  
+  > there is probably a *much* better way of doing this ... PR v. welcome!
+  
+  ## Parameters
+
+  - conn: Map the standard Plug.Conn info see: hexdocs.pm/plug/Plug.Conn.html
+  
+  Returns String with user-agent, IP and language separated by "pipe" charater.
+  """
   def save_user_agent_hash(conn) do
     ua = get_user_agent_string(conn)
     hash = Hash.make(ua, 10)
@@ -35,7 +72,16 @@ defmodule App.Hits do
     hash
   end
 
-  # get_hit_count retrieves the hit count for the given url (hit_path)
+  @doc """
+  get_hit_count/1 reads the log file for the given file path (if it exists)
+  finds the last line in the file and read the hit valie in the last column.
+  
+  ## Parameters
+
+  - hit_path: String the filesystem path to the hits append-only log file.
+  
+  Returns Number count the current hit count for the given url.
+  """
   def get_hit_count(hit_path) do
     exists = File.regular?(hit_path) # check if existing hits log for url
     count = if exists do
@@ -54,8 +100,16 @@ defmodule App.Hits do
     count
   end
 
-  # save_hit is the "main" function for saving a hit including extracting
-  # user-agent data from conn returns count
+  @doc """
+  save_hit/1 is the "main" function for saving a hit including extracting
+  user-agent data from conn (see above).
+  
+  ## Parameters
+
+  - conn: Map the standard Plug.Conn info see: hexdocs.pm/plug/Plug.Conn.html
+  
+  Returns Number count the current hit count for the given url.
+  """
   def save_hit(conn) do
     path = conn.path_info
     hit_path = Path.expand("./logs") <> "/" <>
