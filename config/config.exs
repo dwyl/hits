820ea2649910a0f90891bb100c8d162e2e8a7cf5
Nelson <nelson+github@dwyl.io>
2018-02-27 17:43:38 +0000

    remove "test" from list of pre-commit hooks as it's "covered" by running "coveralls". #10

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :pre_commit, commands: ["test", "credo", "coveralls"]
+config :pre_commit, commands: ["credo", "coveralls"]
 config :app, cowboy_port: 8080
