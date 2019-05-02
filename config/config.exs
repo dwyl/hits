ec28c694e0e44a519667a7380907ad5c4764f02a
nelsonic <nelson+github@dwyl.io>
2019-05-02 23:20:25 +0100

    reset and start fresh. closes https://github.com/dwyl/hits-nodejs/issues/3

--- config/config.exs
+++
@@ -1,6 +0,0 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
-
-config :pre_commit, commands: ["credo", "coveralls"]
-config :app, cowboy_port: 8080
