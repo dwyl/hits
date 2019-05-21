752a89a4095ad6cd4f2018d29abcd2e23e24c696
nelsonic <nelson+github@dwyl.io>
2019-05-21 22:32:57 +0100

    forced to add a file just for bumping coverage of Phoenix generated code which is not being executed ... 🙄 #66

---
+++ test/coverage_test.ex
@@ -0,0 +1,11 @@
+defmodule CoverageTest do
+  use ExUnit.Case
+
+  # Annoyingly, Phoenix creates a bunch of functions that are not executed
+  # during the tests for the app ... this file manually executes them. 
+
+  test "invoke config_change (dummy) function" do
+    assert Hits.Application.config_change("hello", "world", "!") == :ok
+  end
+
+end
