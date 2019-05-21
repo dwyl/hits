db11955279ae93ee0ccd346869a0929167eaa5ad
nelsonic <nelson+github@dwyl.io>
2019-05-21 22:41:21 +0100

    add initial hits_test.exs for #66

---
+++ test/hits_test.exs
@@ -0,0 +1,10 @@
+defmodule HitsTest do
+  use ExUnit.Case
+
+  test "make_badge with default count 1" do
+    badge = Hits.make_badge()
+    # |> IO.inspect(label: "badge")
+    assert badge =~ ~s(1)
+  end
+
+end
