7073193e8d75316c6b55e0d80df0380e8b24db86
nelsonic <nelson+github@dwyl.io>
2019-05-22 08:49:34 +0100

    comment out ErrorHelpers as we are not using Phoenix forms.

--- test/coverage_test.ex
+++ test/coverage_test.ex
@@ -1,11 +1,14 @@
 defmodule CoverageTest do
   use ExUnit.Case
+  # use HitsWeb.ConnCase
 
   # Annoyingly, Phoenix creates a bunch of functions that are not executed
-  # during the tests for the app ... this file manually executes them. 
+  # during the tests for the app ... this file manually executes them.
 
   test "invoke config_change (dummy) function" do
     assert Hits.Application.config_change("hello", "world", "!") == :ok
   end
+
+
 
 end
