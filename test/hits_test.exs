ab43be29cb4b0b00cbc22c413c4b750c15f40b0f
Luis Arteiro <lucho14garfield@gmail.com>
2020-07-22 00:25:44 +0100

    Fixing make_badge for invalid styles test

--- test/hits_test.exs
+++ test/hits_test.exs
@@ -13,8 +13,7 @@ defmodule HitsTest do
   end
 
   test "make_badge with default count 1 and invalid style" do
-    badge = Hits.make_badge(1, "flat")
-    assert badge =~ ~s(flat)
-    refute badge =~ ~s(flat-square)
+    badge = Hits.make_badge(1, "invalid_style")
+    assert badge =~ ~s(flat-square)
   end
 end
