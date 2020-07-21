27dd87c54dfa4fe71aab935b2aa84ab61806cb4e
Luis Arteiro <lucho14garfield@gmail.com>
2020-07-22 00:24:19 +0100

    Forgot to add one test :P

--- test/hits_test.exs
+++ test/hits_test.exs
@@ -11,4 +11,10 @@ defmodule HitsTest do
     assert badge =~ ~s(flat)
     refute badge =~ ~s(flat-square)
   end
+
+  test "make_badge with default count 1 and invalid style" do
+    badge = Hits.make_badge(1, "flat")
+    assert badge =~ ~s(flat)
+    refute badge =~ ~s(flat-square)
+  end
 end
