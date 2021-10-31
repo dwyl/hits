b015cda8724390950b882c5af00bf9ba078df4cc
nelsonic <nelson+github@dwyl.com>
2021-10-31 23:07:14 +0000

    attempt to hard-code websocket url: https://github.com/dwyl/hits/pull/128#issuecomment-955808136

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -8,7 +8,9 @@
 // from the params if you are not using authentication.
 import {Socket} from "phoenix"
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+// let socket = new Socket("/socket", {params: {token: window.userToken}})
+// https://stackoverflow.com/a/67305310/1148249
+let socket = new Socket("wss://hits.dwyl.com/socket/websocket?vsn=2.0.0")
 
 // Connect to the socket:
 socket.connect()
