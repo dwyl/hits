fb28c2f99cdf1613ac9175e01e5080d756ed65bd
nelsonic <nelson+github@dwyl.com>
2021-10-31 20:42:59 +0000

    create config/runtime.exs see: https://fly.io/docs/getting-started/elixir/#runtime-config for #128

---
+++ config/runtime.exs
@@ -0,0 +1,37 @@
+import Config
+
+if config_env() == :prod do
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
+  config :hello_elixir, HelloElixirWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
+    http: [
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    secret_key_base: secret_key_base
+
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :hello_elixir, HelloElixir.Repo,
+    url: database_url,
+    # IMPORTANT: Or it won't find the DB server
+    socket_options: [:inet6],
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+end
