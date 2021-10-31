5ac4f7d11da94b99f7c58fa374b30e12073ffe83
nelsonic <nelson+github@dwyl.com>
2021-10-31 21:36:52 +0000

    fix @app name in releases.ex from :hello_elixir to :hits (derp!) #128

--- lib/hits/release.ex
+++ lib/hits/release.ex
@@ -3,7 +3,7 @@ defmodule Hits.Release do
   Used for executing DB release tasks when run in production without Mix
   installed.
   """
-  @app :hello_elixir
+  @app :hits
 
   def migrate do
     load_app()
